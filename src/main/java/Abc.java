588238c18193f99b334fbfe6f1466e0ffb138fc6
lihe <lihea@cnsuning.com>
2019-04-23 20:08:58 +0800

    delete 3

--- src/main/java/Abc.java
+++ src/main/java/Abc.java
@@ -6,6 +6,7 @@ public class Abc {
     public static void main(String[] args) {
         System.out.println("DDD");
         System.out.println("AAA");
+        System.out.println("EEE");
         System.out.println("BBB");
         System.out.println("CCC");
         System.out.println("DDD");
--- src/main/java/Avaaa.java
+++ src/main/java/Avaaa.java
@@ -9,6 +9,7 @@
  */
 public class Avaaa {
     public static void main(String[] args) {
+        System.out.println("EEE");
         System.out.println("DDD");
     }
 }
