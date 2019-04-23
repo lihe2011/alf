d10e933cb60147525ff84a2533a48a9350c1a1bc
lihe <lihea@cnsuning.com>
2019-04-23 20:04:45 +0800

    delete

--- src/main/java/Abc.java
+++ src/main/java/Abc.java
@@ -5,14 +5,7 @@
 public class Abc {
     public static void main(String[] args) {
         System.out.println("AAA");
-        System.out.println(":");
-        System.out.println(":");
-        System.out.println("1");
-        System.out.println("111");
-        System.out.println("111222");
-        System.out.println();
-        System.out.println("111");
-        System.out.println("111");
+        System.out.println("BBB");
     }
 
     public void aaa(){
