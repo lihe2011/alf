5980970702c73381cac4b7ec16e4fe2c0a9c9371
lihe <lihea@cnsuning.com>
2019-04-23 16:25:33 +0800

    ccc范德萨发
    Signed-off-by: lihe <lihea@cnsuning.com>

--- src/main/java/Abc.java
+++ src/main/java/Abc.java
@@ -7,11 +7,12 @@
  * @Date: 2019/4/22 20:20
  * @Description:
  */
-public class ABC {
+public class Abc {
     public static void main(String[] args) {
         System.out.println("AAA");
         System.out.println(":");
         System.out.println(":");
+        System.out.println("1");
         System.out.println();
     }
 
